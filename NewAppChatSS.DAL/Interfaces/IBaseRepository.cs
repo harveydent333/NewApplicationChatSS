d720e970ff03493907b6a13b44fdd9c86f59d270
vladislav melnik <vladislav.melnik1@simbirsoft.com>
2020-12-29 10:59:07 +0400

    Реализация базового репозитория

--- NewAppChatSS.DAL/Interfaces/IBaseRepository.cs
+++ NewAppChatSS.DAL/Interfaces/IBaseRepository.cs
@@ -11,26 +11,20 @@ namespace NewAppChatSS.DAL.Interfaces
         where TModel : IBaseModel<TEntity, TKey, TContext>, new()
         where TEntity : IEntityBase<TKey>, new()
     {
+        Task<List<TEntity>> GetAsync(TModel model);
+
         Task<TEntity> GetFirstOrDefaultAsync(TModel model);
 
         Task<TEntity> GetLastOrDefaultAsync(TModel model);
 
         Task<long> GetCountAsync(TModel model);
 
-        Task<List<TEntity>> GetAsync(TModel model);
-
         Task<bool> IsExistAsync(TModel model);
 
-        Task DeleteAsync(TEntity entity);
-
-        Task DeleteAsync(IEnumerable<TEntity> entities);
+        Task AddAsync(TEntity entity);
 
         Task ModifyAsync(TEntity entity);
 
-        Task ModifyAsync(IEnumerable<TEntity> entities);
-
-        Task AddAsync(IEnumerable<TEntity> entities);
-
-        Task AddAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
     }
 }
--- NewAppChatSS.DAL/Repositories/BaseRepository.cs
+++ NewAppChatSS.DAL/Repositories/BaseRepository.cs
@@ -33,155 +33,46 @@ namespace NewAppChatSS.DAL.Repositories
             return await query.ToListAsync().ConfigureAwait(false);
         }
 
-        public async virtual Task<long> GetCountAsync(TModel model)
-        {
-            throw new NotImplementedException();
-        }
-
         public async virtual Task<TEntity> GetFirstOrDefaultAsync(TModel model)
         {
-            throw new NotImplementedException();
+            var query = model.GetQuarable(Context);
+            return await query.FirstOrDefaultAsync().ConfigureAwait(false);
         }
 
         public async virtual Task<TEntity> GetLastOrDefaultAsync(TModel model)
         {
-            throw new NotImplementedException();
+            var query = model.GetQuarable(Context);
+            return await query.OrderByDescending(x => x.Id).FirstOrDefaultAsync().ConfigureAwait(false);
+        }
+
+        public async virtual Task<long> GetCountAsync(TModel model)
+        {
+            var query = model.GetQuarable(Context);
+            return await query.CountAsync().ConfigureAwait(false);
         }
 
         public async virtual Task<bool> IsExistAsync(TModel model)
         {
-            throw new NotImplementedException();
-        }
-
-        public async virtual Task AddAsync(IEnumerable<TEntity> rows)
-        {
-            throw new NotImplementedException();
+            var query = model.GetQuarable(Context);
+            return await query.AnyAsync().ConfigureAwait(false);
         }
 
         public async virtual Task AddAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            await EntityOriginal.AddAsync(entity).ConfigureAwait(false);
+            await Context.SaveChangesAsync().ConfigureAwait(false);
         }
 
         public async virtual Task ModifyAsync(TEntity entity)
         {
-            throw new NotImplementedException();
-        }
-
-        public async virtual Task ModifyAsync(IEnumerable<TEntity> entities)
-        {
-            throw new NotImplementedException();
+            EntityOriginal.Update(entity);
+            await Context.SaveChangesAsync().ConfigureAwait(false);
         }
 
         public async virtual Task DeleteAsync(TEntity entity)
         {
-            throw new NotImplementedException();
-        }
-
-        public async virtual Task DeleteAsync(IEnumerable<TEntity> entities)
-        {
-            EntityOriginal.RemoveRange(entities);
+            EntityOriginal.Remove(entity);
             await Context.SaveChangesAsync().ConfigureAwait(false);
         }
     }
-}/*
-
-    protected string TableName => typeof(TEntity).Name;
-
-    protected DbSet<TEntity> EntityOriginal { get; }
-
-    protected virtual IQueryable<TEntity> Entity => EntityOriginal;
-
-    protected TContext Context { get; }
-
-    public virtual async Task<TEntity> GetFirstOrDefaultAsync(TModel model)
-    {
-        var query = model.GetQuarable(Context);
-        return await query.FirstOrDefaultAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task<TEntity> GetLastOrDefaultAsync(TModel model)
-    {
-        var query = model.GetQuarable(Context);
-        return await query.OrderByDescending(x => x.Id).FirstOrDefaultAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task<long> GetCountAsync(TModel model)
-    {
-        var query = model.GetQuarable(Context);
-        return await query.CountAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task<List<TEntity>> GetAsync(TExtendedModel model)
-    {
-        var query = model.GetQuarable(Context);
-        return await query.ToListAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task<bool> IsExistAsync(TModel model)
-    {
-        var query = model.GetQuarable(Context);
-        return await query.AnyAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task AddAsync(TEntity row)
-    {
-        row.CreatedAt = DateTime.Now;
-        row.UpdatedAt = row.CreatedAt;
-
-        await EntityOriginal.AddAsync(row).ConfigureAwait(false);
-        await Context.SaveChangesAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task AddAsync(IEnumerable<TEntity> rows)
-    {
-        foreach (var row in rows)
-        {
-            row.CreatedAt = DateTime.Now;
-            row.UpdatedAt = row.CreatedAt;
-        }
-
-        await EntityOriginal.AddRangeAsync(rows).ConfigureAwait(false);
-        await Context.SaveChangesAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task ModifyAsync(TEntity row)
-    {
-        row.UpdatedAt = DateTime.Now;
-
-        // NOTE: Внедрена единая логика работы с изменениям между методами ModifyAsync, ModifyAsync
-        EntityOriginal.Update(row);
-        await Context.SaveChangesAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task ModifyAsync(IEnumerable<TEntity> rows)
-    {
-        foreach (var row in rows)
-        {
-            row.UpdatedAt = DateTime.Now;
-        }
-
-        EntityOriginal.UpdateRange(rows);
-        await Context.SaveChangesAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task DeleteAsync(TModel model)
-    {
-        var query = model.GetQuarable(Context);
-        EntityOriginal.RemoveRange(query);
-
-        await Context.SaveChangesAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task DeleteAsync(TEntity entity)
-    {
-        EntityOriginal.Remove(entity);
-        await Context.SaveChangesAsync().ConfigureAwait(false);
-    }
-
-    public virtual async Task DeleteAsync(IEnumerable<TEntity> entities)
-    {
-        EntityOriginal.RemoveRange(entities);
-        await Context.SaveChangesAsync().ConfigureAwait(false);
-    }
-*/
+}
